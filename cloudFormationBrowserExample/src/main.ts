17c6dc0d9ae078fb40d63298c07dfa40d15bf7cb
Willems <rwillems@amazon.com>
2020-01-19 20:08:42 +0100

    Removed allowed pattern from example

--- cloudFormationBrowserExample/src/main.ts
+++ cloudFormationBrowserExample/src/main.ts
@@ -57,7 +57,6 @@ const jsonValue =
        "MinLength": "9",
        "MaxLength": "18",
        "Default": "0.0.0.0/0",
-       "AllowedPattern": "(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})/(\\d{1,2})",
        "ConstraintDescription": "must be a valid IP CIDR range of the form x.x.x.x/x."
      }
    },
